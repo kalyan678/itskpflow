803031560886b8a13ebeb139249631a4639cf0ad
itskpflow <46974898+kalyan678@users.noreply.github.com>
2026-09-04 21:39:56 +0530

    Stage 1: clean portfolio foundation and remove starter content

--- data/talkData.ts
+++ data/talkData.ts
@@ -5,21 +5,6 @@ interface Talk {
   imgSrc?: string
 }
 
-const talkData: Talk[] = [
-  {
-    title: 'Hello World Project',
-    description: `This is a simple hello world program. It prints "Hello, World!" to the console.
-    It is a simple program that demonstrates the basic syntax of a programming language.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'Hello World Project 2',
-    description: `This is a simple hello world program. It prints "Hello, World!" to the console.
-    It is a simple program that demonstrates the basic syntax of a programming language.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
-]
+const talkData: Talk[] = []
 
 export default talkData
